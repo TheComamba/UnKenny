f78c5864dc02cd7065fba8697cae6cf62da6d31c
Simon Heinze <simon@jojoheinze.de>
2023-04-10 18:42:35 +0200

    Third attempt at passing data between js and handlebars.

--- src/apps/edit-npc.js
+++ src/apps/edit-npc.js
@@ -6,7 +6,6 @@ export default class EditNPCApp extends FormApplication {
 
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
-            classes: ['form'],
             id: "edit-npc",
             title: "Edit UnKenny NPC",
             template: `modules/unkenny-llm-npc/apps/edit-npc.hbs`,
