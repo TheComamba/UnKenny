a6c8667dfb46169891682960238000b67f1d5723
Simon Heinze <simon@jojoheinze.de>
2023-04-10 18:04:09 +0200

    First attempt at passing data between js and handlebars.

--- src/apps/edit-npc.js
+++ src/apps/edit-npc.js
@@ -1,4 +1,7 @@
 export default class EditNPCApp extends FormApplication {
+    name = "gabi";
+    preamble = "ken";
+
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
             id: "edit-npc",
@@ -8,6 +11,15 @@ export default class EditNPCApp extends FormApplication {
         });
     }
 
+    //override
+    getData() {
+        return {
+            name: this.name,
+            preamble: this.preamble,
+        };
+    }
+
+    //override
     activateListeners(html) {
         super.activateListeners;
         html.find("button.cancel").on("click", this._onClickCancelButton.bind(this));
@@ -16,9 +28,13 @@ export default class EditNPCApp extends FormApplication {
 
     async _onClickCancelButton(event) {
         console.log("Edit UnKenny NPC cancelled.");
+        console.log(this.name);
+        console.log(this.preamble);
     }
 
     async _onClickSaveButton(event) {
         console.log("Saving UnKenny NPC.");
+        console.log(this.name);
+        console.log(this.preamble);
     }
 }
