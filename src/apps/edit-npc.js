2572b1901cdd6846b6cbe3770f450d278256eab4
Simon Heinze <simon@jojoheinze.de>
2023-04-10 08:16:36 +0200

    Basic layout of edit enkenny npc form.

--- src/apps/edit-npc.js
+++ src/apps/edit-npc.js
@@ -1,11 +1,10 @@
-export default class EditNPCApp extends Application {
+export default class EditNPCApp extends FormApplication {
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
             id: "edit-npc",
             title: "Edit UnKenny NPC",
             template: `modules/unkenny-llm-npc/apps/edit-npc.hbs`,
             width: 720,
-            height: 720,
         });
     }
 }
