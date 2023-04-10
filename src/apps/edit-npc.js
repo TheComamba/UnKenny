6adb22f4b9842cead20f14b1ffdbbd8316180144
Simon Heinze <simon@jojoheinze.de>
2023-04-10 18:19:34 +0200

    Second attempt at passing data between js and handlebars.

--- src/apps/edit-npc.js
+++ src/apps/edit-npc.js
@@ -1,11 +1,24 @@
 export default class EditNPCApp extends FormApplication {
+    constructor(npc) {
+        super();
+        this.npc = npc;
+    }
+
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ['form'],
             id: "edit-npc",
             title: "Edit UnKenny NPC",
             template: `modules/unkenny-llm-npc/apps/edit-npc.hbs`,
             width: 720,
         });
+    }
+
+    //override
+    getData() {
+        return {
+            npc: this.npc,
+        };
     }
 
     //override
@@ -17,9 +30,18 @@ export default class EditNPCApp extends FormApplication {
 
     async _onClickCancelButton(event) {
         console.log("Edit UnKenny NPC cancelled.");
+        console.log(this.npc.name);
+        console.log(this.npc.preamle);
     }
 
     async _onClickSaveButton(event) {
         console.log("Saving UnKenny NPC.");
+        console.log(this.npc.name);
+        console.log(this.npc.preamle);
+    }
+
+    //override
+    async _updateObject(event, formData) {
+        console.log("Updating UnKenny NPC form.");
     }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,16 @@
 import EditNPCApp from "../apps/edit-npc.js";
+import UnKennyNPC from "../data/npc.js";
 
 Hooks.on("renderSidebarTab", async (app, html) => {
     if (app.options.id == "actors") {
         let button = $("<button class='unkenny-npc-button'>UnKenny NPC</button>")
 
+        let exampleNPC = new UnKennyNPC;
+        exampleNPC.name = "gabi";
+        exampleNPC.preamble = "ken";
+
         button.click(function () {
-            new EditNPCApp().render(true);
+            new EditNPCApp(exampleNPC).render(true);
         });
 
         html.find(".directory-footer").append(button);
