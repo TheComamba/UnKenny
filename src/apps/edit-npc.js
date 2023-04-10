f8547c3573908bb0042247d5cf78c579003aa506
Simon Heinze <simon@jojoheinze.de>
2023-04-10 07:54:34 +0200

    Loading hbs template for app window.

--- src/apps/edit-npc.js
+++ src/apps/edit-npc.js
@@ -3,7 +3,7 @@ export default class EditNPCApp extends Application {
         return foundry.utils.mergeObject(super.defaultOptions, {
             id: "edit-npc",
             title: "Edit UnKenny NPC",
-            template: `edit-npc.hbs`,
+            template: `modules/unkenny-llm-npc/apps/edit-npc.hbs`,
             width: 720,
             height: 720,
         });
