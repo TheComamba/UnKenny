d281b70a548d5945afd0e18d2afbea06c57e5673
Simon Heidrich <simon@jojoheinze.de>
2023-04-16 21:09:13 +0200

    Trying to register sheet for UnKenny.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,12 @@
 import UnKennyNPCModel from "../data/npc.js";
+import UnKennyNPCSheet from "../apps/unkenny-npc-sheet.js";
 
 Hooks.on("init", () => {
     Object.assign(CONFIG.Actor.dataModels, {
         "unkenny-llm-npc.UnKennyNPC": UnKennyNPCModel
+    });
+    DocumentSheetConfig.registerSheet(Actor, "unkenny-llm-npc", UnKennyNPCSheet, {
+        types: ["unkenny-llm-npc.UnKennyNPC"],
+        makeDefault: true
     });
 });
