6ad1d62090a109013dd2e26589e6afbfd769d9fa
Simon Heidrich <simon@jojoheinze.de>
2023-04-16 20:43:28 +0200

    Turning UnKennyNPC into a TypeDataModel.

--- src/data/npc.js
+++ src/data/npc.js
@@ -1,11 +1,9 @@
-export default class UnKennyNPC {
-    name;
-    preamble;
-
-    static async create(params) {
-        let unKenny = new UnKennyNPC();
-        unKenny.name = params["name"];
-        unKenny.preamble = params["preamble"];
-        return unKenny;
-    }
+export default class UnKennyNPC extends foundry.abstract.TypeDataModel {
+    static defineSchema() {
+        const fields = foundry.data.fields;
+        return {
+          name: new fields.StringField({required: true, blank: false}),
+          preamble: new fields.StringField({required: false, blank: false})
+        }
+      }
 }
