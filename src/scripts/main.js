b88176abd0b497df4d632730ed46936aafffdff7
Simon Heidrich <simon@jojoheinze.de>
2023-04-16 20:28:03 +0200

    Deleting sidebar tab, because creating a primary document type is currently impossible.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,58 +1,6 @@
 import EditNPCApp from "../apps/edit-npc.js";
 import GLOBALS from "./shared.js";
 import UnKennyNPC from "../data/npc.js";
-import UnKennySidebarDirectory from "./sidebar-directory.js";
-
-function widenSidebar(html) {
-    ui.unkenny.render(true);
-
-    html[0]
-        .querySelector("#sidebar-tabs")
-        .style.setProperty(
-            "--sidebar-tab-width",
-            `${Math.floor(
-                parseInt(getComputedStyle(html[0]).getPropertyValue("--sidebar-width")) /
-                (document.querySelector("#sidebar-tabs").childElementCount + 1)
-            )}px`
-        );
-}
-
-function createUnKennyNpcTab(html) {
-    const tab = document.createElement("a");
-    tab.classList.add("item");
-    tab.dataset.tab = GLOBALS.ID;
-    tab.dataset.tooltip = GLOBALS.NAME;
-    if (!("tooltip" in game)) tab.title = GLOBALS.NAME;
-
-    const icon = document.createElement("i");
-    icon.setAttribute("class", CONFIG.Macro.sidebarIcon);
-    tab.append(icon);
-
-    if (!document.querySelector(`#sidebar-tabs > [data-tab='${GLOBALS.ID}']`)) {
-        document.querySelector("#sidebar-tabs > [data-tab='actors']").after(tab);
-    }
-}
-
-Hooks.on("init", () => {
-    CONFIG.ui.unkenny = UnKennySidebarDirectory;
-
-    game.settings.register(GLOBALS.ID, "clickExecute", {
-        name: `${GLOBALS.ID}.settings.clickExecute.name`,
-        hint: `${GLOBALS.ID}.settings.clickExecute.hint`,
-        scope: "client",
-        config: true,
-        type: Boolean,
-        default: true,
-        onChange: () => {
-            ui.unkenny.render();
-        },
-    });
-});
-
-Hooks.on("renderSidebar", (_app, html) => {
-    widenSidebar(html);
-    createUnKennyNpcTab(html);
-});
 
 Hooks.on("renderSidebarTab", async (app, html) => {
     if (app.tabName == GLOBALS.ID) {
