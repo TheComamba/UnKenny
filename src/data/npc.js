a01afba806b48e154791b99f512ff90496632f8c
Simon Heidrich <simon@jojoheinze.de>
2023-04-17 18:10:32 +0200

    Deleting everything needed for subtyping.

--- src/data/npc.js
+++
@@ -1,9 +0,0 @@
-export default class UnKennyNPCModel extends foundry.abstract.TypeDataModel {
-    static defineSchema() {
-        const fields = foundry.data.fields;
-        return {
-            name: new fields.StringField({ required: false, blank: false }),
-            preamble: new fields.StringField({ required: false, blank: false })
-        };
-    }
-}
