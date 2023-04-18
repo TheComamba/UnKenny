728ae13b93777e229d5523532bee686be53a37be
Simon Heinze <simon@jojoheinze.de>
2023-04-18 22:03:18 +0200

    Adjust button text to unkennyness.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
 import UnKennySheet from "../apps/unkenny-sheet.js";
+import isUnkenny from "./unkenny.js";
 
 Hooks.on("getActorSheetHeaderButtons", (sheet, buttons) => {
+  let buttonText = isUnkenny(sheet.object) ? "Modify UnKennyness" : "Make UnKenny";
   buttons.unshift({
-    label: "Modify UnKennyness",
+    label: buttonText,
     class: "modify-unkennyness",
     icon: "fas fa-user",
     onclick: () => {
