7ff7c707c1ab8b658d49afe28d818ad09679ced0
TheComamba <thecomamba@jojoheinze.de>
2023-06-04 18:52:37 +0200

    First implementation of a Rest request.

--- src/scripts/llm.js
+++ src/scripts/llm.js
@@ -1,6 +1,21 @@
+const SERVER_URL = "http://127.0.0.1:23308";
+
 function generateResponse(actor, input) {
-    //TODO: LLM Magic goes here.
+    const preamble = "TODO";
+    postRequest(JSON.stringify({
+        preamble: preamble,
+        input: input
+    })).then(response => {
+        console.log(response);
+    });
     return "[Some response]";
+}
+
+function postRequest(body) {
+    return fetch(SERVER_URL, {
+        method: "POST",
+        body: body
+    }).then(response => response.json());
 }
 
 export { generateResponse };
