a5d0f816768853cfd24066efc7e774b02f4f23ca
TheComamba <thecomamba@jojoheinze.de>
2023-12-04 21:00:52 +0100

    Using Auto* classes to generate response.

--- src/scripts/llm.js
+++ src/scripts/llm.js
@@ -1,18 +1,17 @@
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.9.0';
+import { AutoModelForCausalLM, AutoTokenizer } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.9.0';
 // There are not many options at the moment:
 // https://huggingface.co/models?pipeline_tag=text-generation&library=transformers.js&language=en&sort=trending
-const model = 'Felladrin/onnx-bloomz-560m-sft-chat';
-//const model = 'mkly/TinyStories-1M-ONNX';
+const model_path = 'Felladrin/onnx-bloomz-560m-sft-chat';
+//const model_path = 'mkly/TinyStories-1M-ONNX';
 
 async function generateResponse(actor, input) {
-    let response_generator = await pipeline('text-generation', model);
-    let response = await response_generator(input);
-    if (response.length === 0) {
-        return "[Response is empty.]"
-    }
-    let response_text = response[0].generated_text;
-    response_text = response_text.substring(input.length);
-    return response_text;
+    const model = await AutoModelForCausalLM.from_pretrained(model_path);
+    const tokenizer = await AutoTokenizer.from_pretrained(model_path);
+    let { input_ids } = tokenizer('</s>' + input + '<s>');
+    let tokens = await model.generate(input_ids, { max_new_tokens: 128, repetition_penalty: 1.2});
+    let response = tokenizer.decode(tokens[0], { skip_special_tokens: true });
+    response = response.substring(input.length);
+    return response;
 }
 
 export { generateResponse };
