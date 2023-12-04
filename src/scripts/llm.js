dfc27d258ac69d45710ace4a6f0f2f09c4935c66
TheComamba <thecomamba@jojoheinze.de>
2023-12-04 20:08:31 +0100

    Using default huggingface text generator.

--- src/scripts/llm.js
+++ src/scripts/llm.js
@@ -1,6 +1,14 @@
-function generateResponse(actor, input) {
-    //TODO: LLM Magic goes here.
-    return "[Some response]";
+import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.9.0';
+
+async function generateResponse(actor, input) {
+    let response_generator = await pipeline('text-generation');
+    let response = await response_generator(input);
+    if (response.length === 0) {
+        return "[Response is empty.]"
+    }
+    let response_text = response[0].generated_text;
+    response_text = response_text.substring(input.length);
+    return response_text;
 }
 
 export { generateResponse };
