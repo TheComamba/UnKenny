6a70a0f6f6787c47a49e5af289469ba67d1f41b0
TheComamba <thecomamba@jojoheinze.de>
2023-12-04 20:44:57 +0100

    Trying to change model with pipeline API.

--- src/scripts/llm.js
+++ src/scripts/llm.js
@@ -1,7 +1,11 @@
 import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.9.0';
+// There are not many options at the moment:
+// https://huggingface.co/models?pipeline_tag=text-generation&library=transformers.js&language=en&sort=trending
+const model = 'Felladrin/onnx-bloomz-560m-sft-chat';
+//const model = 'mkly/TinyStories-1M-ONNX';
 
 async function generateResponse(actor, input) {
-    let response_generator = await pipeline('text-generation');
+    let response_generator = await pipeline('text-generation', model);
     let response = await response_generator(input);
     if (response.length === 0) {
         return "[Response is empty.]"
