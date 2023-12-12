700546c9161f2e0511453972fad9976f022cedcd
TheComamba <thecomamba@jojoheinze.de>
2023-12-12 22:03:28 +0100

    Setting stored model as selected.

--- src/apps/unkenny-sheet.js
+++ src/apps/unkenny-sheet.js
@@ -14,14 +14,28 @@ class UnKennySheet extends FormApplication {
         const context = await super.getData(options);
         context.preamble = this.actor.getFlag("unkenny", "preamble");
         context.models = [
-            "Felladrin/onnx-bloomz-560m-sft-chat",
-            "mkly/TinyStories-1M-ONNX"
+            { name: "" },
+            { name: "Felladrin/onnx-bloomz-560m-sft-chat" },
+            { name: "mkly/TinyStories-1M-ONNX" }
         ];
+        let model = this.actor.getFlag("unkenny", "model");
+        if (!model) {
+            model = "";
+        }
+        context.models.forEach(m => {
+            if (m.name == model) {
+                m.isSelected = true;
+            }
+        });
         return context;
     }
 
     async _updateObject(_event, formData) {
         await this.actor.setFlag("unkenny", "preamble", formData.preamble);
+        
+        const selectedModel = formData.models.find(m => m.isSelected)?.name;
+        await this.actor.setFlag("unkenny", "model", selectedModel);
+        
         await updateMacro(this.actor);
     }
 }
