36846fc8cd3fefe3d9bd5e6079ebcd988a4e8f57
TheComamba <thecomamba@jojoheinze.de>
2023-12-23 08:23:13 +0100

    Removing json conversions.

--- src/apps/unkenny-sheet.js
+++ src/apps/unkenny-sheet.js
@@ -14,33 +14,33 @@ class UnKennySheet extends DocumentSheet {
     async getData(options = {}) {
         if (Object.keys(this.context).length === 0) {
             this.context = await super.getData(options);
+            this.initContextModels();
             this.initContextWithActorData();
         }
 
         return this.context;
     }
 
-    initContextWithActorData() {
-        this.context.preamble = this.object.getFlag("unkenny", "preamble") || "";
-
+    initContextModels() {
         // Models to choose from: 
         // https://huggingface.co/models?pipeline_tag=text-generation&library=transformers.js&sort=trending
         this.context.models = [
             { text: "", value: "{}" },
-            { text: "Felladrin Bloomz (ca. 3 GB RAM)", value: JSON.stringify({ path: "Felladrin/onnx-bloomz-560m-sft-chat", type: "local" }) },
-            { text: "Felladrin GPT2 (ca. 1 GB RAM)", value: JSON.stringify({ path: "Felladrin/onnx-gpt2-conversational-retrain", type: "local" }) },
-            { text: "Felladrin GPT2 Large (ca. 2 GB RAM)", value: JSON.stringify({ path: "Felladrin/onnx-gpt2-large-conversational-retrain", type: "local" }) },
-            { text: "Xenova GPT2 Large (ca. 2 GB RAM)", value: JSON.stringify({ path: "Xenova/gpt2-large-conversational", type: "local" }) },
-            { text: "OpenAI GPT-3.5-turbo", value: JSON.stringify({ path: "gpt-3.5-turbo", type: "api" }) },
-            { text: "OpenAI GPT-4-preview", value: JSON.stringify({ path: "gpt-4-1106-preview", type: "api" }) }
+            { text: "Felladrin Bloomz (ca. 3 GB RAM)", path: "Felladrin/onnx-bloomz-560m-sft-chat", type: "local" },
+            { text: "Felladrin GPT2 (ca. 1 GB RAM)", path: "Felladrin/onnx-gpt2-conversational-retrain", type: "local" },
+            { text: "Felladrin GPT2 Large (ca. 2 GB RAM)", path: "Felladrin/onnx-gpt2-large-conversational-retrain", type: "local" },
+            { text: "Xenova GPT2 Large (ca. 2 GB RAM)", path: "Xenova/gpt2-large-conversational", type: "local" },
+            { text: "OpenAI GPT-3.5-turbo", path: "gpt-3.5-turbo", type: "api" },
+            { text: "OpenAI GPT-4-preview", path: "gpt-4-1106-preview", type: "api" }
         ];
-        let currentModel = this.object.getFlag("unkenny", "model") || "";
-        this.context.models.forEach(m => {
-            if (JSON.parse(m.value).path == currentModel) {
-                m.isSelected = true;
-            }
-        });
+    }
+
+    initContextWithActorData() {
+        this.context.preamble = this.object.getFlag("unkenny", "preamble") || "";
         this.context.llmAPIKey = this.object.getFlag("unkenny", "llmAPIKey") || "";
+
+        let currentModel = this.object.getFlag("unkenny", "model") || "";
+        this.setContextModel(currentModel);
 
         this.context.minNewTokens = this.object.getFlag("unkenny", "minNewTokens") || 3;
         this.context.maxNewTokens = this.object.getFlag("unkenny", "maxNewTokens") || 128;
@@ -53,13 +53,8 @@ class UnKennySheet extends DocumentSheet {
         await super._onChangeInput(event);
         let needsReRendering = false;
         if (event.target.name == "model") {
-            this.context.models.forEach(m => {
-                if (m.value == event.target.value) {
-                    m.isSelected = true;
-                } else {
-                    m.isSelected = false;
-                }
-            });
+            let model = event.target.value
+            this.setContextModel(model);
             needsReRendering = true;
         }
         if (needsReRendering) {
@@ -67,10 +62,20 @@ class UnKennySheet extends DocumentSheet {
         }
     }
 
+    setContextModel(model) {
+        this.context.models.forEach(m => {
+            if (m.path == model) {
+                m.isSelected = true;
+            } else {
+                m.isSelected = false;
+            }
+        });
+    }
+
     async _updateObject(_event, formData) {
         await this.object.setFlag("unkenny", "preamble", formData.preamble);
-        await this.object.setFlag("unkenny", "model", JSON.parse(formData.model).path);
-        await this.object.setFlag("unkenny", "llmType", JSON.parse(formData.model).type);
+        await this.object.setFlag("unkenny", "model", formData.model);
+        // await this.object.setFlag("unkenny", "llmType", JSON.parse(formData.model).type);
         await this.object.setFlag("unkenny", "llmAPIKey", formData.llmAPIKey);
         await this.object.setFlag("unkenny", "minNewTokens", formData.minNewTokens);
         await this.object.setFlag("unkenny", "maxNewTokens", formData.maxNewTokens);
