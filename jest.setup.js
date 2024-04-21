7e4bc111aee059205959e4cfcaf5cec5a49829c1
TheComamba <thecomamba@jojoheinze.de>
2024-04-21 11:15:02 +0200

    Mocking Actor.

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,6 @@
-global.DocumentSheet = require('./__mocks__/document-sheet.js').default;
+global.Actor = require('./__mocks__/actor.js').default;
 global.Dialog = require('./__mocks__/dialog.js').default;
+global.DocumentSheet = require('./__mocks__/document-sheet.js').default;
 global.Hooks = require('./__mocks__/hooks.js').default;
 
 beforeEach(() => {
---
+++ src/scripts/shared.test.js
@@ -0,0 +1,31 @@
+import { isUnkenny } from './shared.js';
+
+describe('isUnkenny', () => {
+    let ui;
+    beforeEach(() => {
+        ui = {
+            notifications: {
+                error: jest.fn()
+            }
+        };
+        global.ui = ui;
+    });
+
+    it('should return false and show error when actor is null', () => {
+        const result = isUnkenny(null);
+        expect(result).toBe(false);
+        expect(ui.notifications.error).toHaveBeenCalledWith("Unkennyness checked for null actor.");
+    });
+
+    it('should return false when actor does not have unkenny flag', () => {
+        const actor = new Actor();
+        expect(isUnkenny(actor)).toBe(false);
+    });
+
+    it('should return true when actor has unkenny flag', () => {
+        const actor = new Actor();
+        actor.setFlag("unkenny", "alias", 'some-alias');
+        expect(isUnkenny(actor)).toBe(true);
+    });
+});
+// Path: src/scripts/chat-message-parsing.test.js
