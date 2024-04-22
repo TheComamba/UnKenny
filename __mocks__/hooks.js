1649e05f18ff145b6b33449577844cba2ebc5678
TheComamba <thecomamba@jojoheinze.de>
2024-04-22 19:00:29 +0200

    Registered yet unused game settings as todos.

--- __mocks__/hooks.js
+++ __mocks__/hooks.js
@@ -2,6 +2,7 @@ import { jest } from '@jest/globals';
 
 const Hooks = {
   on: jest.fn(),
+  once: jest.fn(),
 };
 
 Hooks.on.mockImplementation((event, callback) => {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,151 @@ import { postResponse } from "./llm.js";
 
 // CONFIG.debug.hooks = true;
 
+Hooks.once('init', async function () {
+  game.settings.register("unkenny", "model", {
+    name: "Large Language Model",
+    hint: "TODO",
+    scope: "world",
+    config: true,
+    type: String,
+    choices: {
+      "a": "Model A",
+      "b": "Model B",
+    },
+    default: "a",
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "apiKey", {
+    name: "OpenAI API Key",
+    hint: "TODO",
+    scope: "world",
+    config: true,
+    type: String,
+    default: "",
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "minNewTokens", {
+    name: "Minimum Number of New Tokens",
+    hint: "TODO",
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0,
+      max: 100,
+      step: 1
+    },
+    default: 50,
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "maxNewTokens", {
+    name: "Maximum Number of New Tokens",
+    hint: "TODO",
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0,
+      max: 100,
+      step: 1
+    },
+    default: 50,
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "repititionPenalty", {
+    name: "Repetition Penalty / Frequency Penalty",
+    hint: "TODO",
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0,
+      max: 100,
+      step: 10
+    },
+    default: 50,
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "presencePenalty", {
+    name: "Presence Penalty",
+    hint: "TODO: Only OpenAI",
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0,
+      max: 100,
+      step: 10
+    },
+    default: 50,
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "temperature", {
+    name: "Temperature",
+    hint: "TODO: Only OpenAI",
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0,
+      max: 2,
+      step: 0.01
+    },
+    default: 1.0,
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "topP", {
+    name: "Top P",
+    hint: "TODO: Only OpenAI",
+    scope: "world",
+    config: true,
+    type: Number,
+    range: {
+      min: 0.1,
+      max: 1,
+      step: 0.01
+    },
+    default: 0.5,
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+  game.settings.register("unkenny", "prefixWithTalk", {
+    name: "Prefix responses with /talk",
+    hint: "TODO",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: false,
+    onChange: value => {
+      console.log(value)
+    }
+  });
+
+});
+
 Hooks.on("getActorSheetHeaderButtons", async (sheet, buttons) => {
   let buttonText = isUnkenny(sheet.object) ? "Modify UnKennyness" : "Make UnKenny";
   buttons.unshift({
