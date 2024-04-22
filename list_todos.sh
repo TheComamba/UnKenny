c791f679c091290afd54af6fd5e413792a2e9734
TheComamba <thecomamba@jojoheinze.de>
2024-04-22 19:22:09 +0200

    Making sure the list todos script does not grep itself.

--- list_todos.sh
+++ list_todos.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-if grep -rin todo * --exclude-dir=node_modules --include={*.js,*.hbs,*.json,*.sh,*.md} -B 3 -A 3; then
+SCRIPT=$(readlink -f "$0")
+
+if grep -rin todo * \
+    --include={*.js,*.hbs,*.json,*.sh,*.md} \
+    --exclude-dir=node_modules \
+    --exclude-dir=coverage \
+    --exclude=$(basename $SCRIPT) \
+    -B 3 -A 3; then
+    echo
     echo "TODOs found!"
     exit 1
 else
