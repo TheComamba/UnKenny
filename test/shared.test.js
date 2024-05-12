fa80bf0c6758c91b2d2eba98477ca4dcc1fb83d5
TheComamba <thecomamba@jojoheinze.de>
2024-05-12 09:45:38 +0200

    Test skeleton. [no ci]

--- test/shared.test.js
+++ test/shared.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { isUnkenny } from '../src/scripts/shared.js';
+import { isUnkenny, postInChat } from '../src/scripts/shared.js';
 
 describe('isUnkenny', () => {
     beforeEach(() => {
@@ -23,4 +23,32 @@ describe('isUnkenny', () => {
         expect(isUnkenny(actor)).to.equal(true);
     });
 });
-// Path: src/scripts/chat-message-parsing.test.js
+
+describe('postInChat', () => {
+    beforeEach(() => {
+        global.ui.reset();
+    });
+
+    it('should post message in chat when originator is User', () => {
+        const user_id = '60d7213e4f5f2b6';
+        postInChat(user_id, 'some message');
+        expect(global.ChatMessage.create.called).to.be.true;
+    });
+
+    it('should post message in chat when originator is Actor', () => {
+        const actor = new Actor();
+        postInChat(actor, 'some message');
+        expect(global.ChatMessage.create.called).to.be.true;
+    });
+
+    it('should show error when originator is not User or Actor', () => {
+        postInChat(null, 'some message');
+        expect(global.ui.notifications.error.called).to.be.true;
+    });
+
+    it('should not post message in chat when chatMessage hook returns false', () => {
+        global.Hooks.call.withArgs("chatMessage").returns(false);
+        postInChat(new User(), 'some message');
+        expect(global.ChatMessage.create.called).to.be.false;
+    });
+});
