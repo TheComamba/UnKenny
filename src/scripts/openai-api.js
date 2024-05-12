38f2aa863abaaf8bb465f2d023307d47e70ef96e
TheComamba <thecomamba@jojoheinze.de>
2024-05-12 17:23:29 +0200

    Using loadExternalModule for openai.

--- src/scripts/openai-api.js
+++ src/scripts/openai-api.js
@@ -1,18 +1,9 @@
-let OpenAi;
-
-import('https://cdn.jsdelivr.net/npm/openai@4.22.1/+esm')
-    .then(module => {
-        OpenAi = module.default;
-    })
-    .catch(error => {
-        if (process.env.NODE_ENV === 'test') {
-            // Maybe add a mock here
-        } else {
-            console.error("Unable to load module", error);
-        }
-    });
+import { loadExternalModule } from './shared';
 
 async function getResponseFromOpenAI(parameters, messages) {
+    const OpenAIModule = await loadExternalModule('openai', '4.22.1/+esm');
+    const OpenAi = OpenAIModule.default;
+
     const openai = new OpenAi({
         apiKey: parameters.apiKey,
         dangerouslyAllowBrowser: true,
@@ -27,12 +18,11 @@ async function getResponseFromOpenAI(parameters, messages) {
     };
     try {
         const chatCompletion = await openai.chat.completions.create(input_parameters);
+        return chatCompletion['choices'][0]['message']['content'];
     } catch (error) {
         ui.notifications.error(`Unable to get response from OpenAI: ${error}`);
         return;
     }
-
-    return chatCompletion['choices'][0]['message']['content'];
 }
 
 export { getResponseFromOpenAI };
