344dbb70a04226fa21b0565940ed402c7901280b
TheComamba <thecomamba@jojoheinze.de>
2024-05-12 08:43:20 +0200

    More models.

--- src/scripts/models.js
+++ src/scripts/models.js
@@ -3,10 +3,12 @@ function getModels() {
     // https://huggingface.co/models?pipeline_tag=text-generation&library=transformers.js&sort=trending
     return [
         { text: "", path: "" },
-        { text: "Local: Felladrin Tiny Mistral Chat v2", path: "Felladrin/onnx-TinyMistral-248M-Chat-v2" },
-        // { text: "Local: Xenova GPT-2 Large Conversational", path: "Xenova/gpt2-large-conversational" }, // Outdated
-        { text: "Local: Xenova Qwen 1.5 Chat", path: "Xenova/Qwen1.5-1.8B-Chat" },
-        //{ text: "Local: Xenova Tiny Lllama 1.1 Chat", path: "Xenova/TinyLlama-1.1B-Chat-v1.0" }, // Proved to be very repetitive.
+        { text: "Local: Felladrin Minueza", path: "Felladrin/onnx-Minueza-32M-Chat" },
+        { text: "Local: Felladrin Tiny Mistral", path: "Felladrin/onnx-TinyMistral-248M-Chat-v2" },
+        // { text: "Local: Schmuell Tiny Llama", path: "schmuell/TinyLlama-1.1B-Chat-v1.0-int4" }, // Cannot be loaded.
+        // { text: "Local: Xenova GPT-2 Large Conversational", path: "Xenova/gpt2-large-conversational" }, // Outdated.
+        { text: "Local: Xenova Qwen", path: "Xenova/Qwen1.5-0.5B-Chat" },
+        //{ text: "Local: Xenova Tiny Llama", path: "Xenova/TinyLlama-1.1B-Chat-v1.0" }, // Proved to be very repetitive.
         { text: "OpenAI: GPT 3.5 turbo", path: "gpt-3.5-turbo" },
         { text: "OpenAI: GPT 4", path: "gpt-4" },
         { text: "OpenAI: GPT 4 turbo", path: "gpt-4-turbo" }
