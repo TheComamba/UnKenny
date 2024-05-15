d47f46b303b08587d7e61f9fc019b16460d3f2e7
TheComamba <98019967+TheComamba@users.noreply.github.com>
2024-05-15 13:41:14 +0200

    Removing database from chat-message.js [no ci]

--- __mocks__/chat-message.js
+++ __mocks__/chat-message.js
@@ -6,8 +6,6 @@ class ChatMessage {
     this.applyChatData(chatData);
   }
 
-  static database = [];
-
   applyChatData(chatData) {
     this.content = chatData.content;
     this.speaker = chatData.speaker;
@@ -21,12 +19,6 @@ class ChatMessage {
     Hooks.call("preCreateChatMessage", newMessage, chatData, options, originator);
     newMessage.applyChatData(chatData);
     Hooks.call("createChatMessage", newMessage, options, originator);
-    ChatMessage.database.push(newMessage);
-  }
-
-  static reset() {
-    // TODO: ChatMessage has no database. ChatLog has a remember method.
-    ChatMessage.database = [];
   }
 }
 
