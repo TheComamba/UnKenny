8df3dd79854a057bacc7648a10b4a3b733ced249
TheComamba <thecomamba@jojoheinze.de>
2024-05-16 06:14:53 +0200

    Fixes.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+if ! command -v sudo &> /dev/null; then
+    echo "sudo is not installed. Installing..."
+    apt-get install -y sudo
+fi
+
+if ! command -v curl &> /dev/null; then
+    echo "curl is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y curl
+fi
+
 if ! command -v nvm &> /dev/null; then
     echo "nvm is not installed. Installing..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
