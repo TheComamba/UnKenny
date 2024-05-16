4bee75df4227e642f0928f6a0c143c6dbc18b9a5
TheComamba <98019967+TheComamba@users.noreply.github.com>
2024-05-16 08:21:14 +0200

    Dev/fix setup script (#67)

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "This script is currently broken. Best install nodejs and npm manually, for the moment."
-exit 1
-
 set -e
 
-# Source nvm script, replace with the path to your nvm script
-# Usually it's in the home directory in .nvm/nvm.sh
-[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+if ! command -v curl &> /dev/null; then
+    echo "curl is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y curl
+fi
 
 if ! command -v nvm &> /dev/null; then
     echo "nvm is not installed. Installing..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-    source ~/.bashrc
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
 
 nvm install 18
@@ -20,3 +20,7 @@ nvm use 18
 
 echo "Ensuring dependencies..."
 npm install
+
+echo Setup finished. You probably need to call
+echo . ~/.bashrc
+echo to refresh your current terminal.
