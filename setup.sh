d1d84f82c065dab63f2da333d7d80fa97fa04bbf
TheComamba <thecomamba@jojoheinze.de>
2024-05-16 05:53:04 +0200

    Fixing setup script.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-echo "This script is currently broken. Best install nodejs and npm manually, for the moment."
-exit 1
-
 set -e
-
-# Source nvm script, replace with the path to your nvm script
-# Usually it's in the home directory in .nvm/nvm.sh
-[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
 
 if ! command -v nvm &> /dev/null; then
     echo "nvm is not installed. Installing..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-    source ~/.bashrc
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
 
 nvm install 18
