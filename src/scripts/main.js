84239434b73ffa2273a99805473ab74512cea828
TheComamba <thecomamba@jojoheinze.de>
2024-06-29 07:30:59 +0200

    Some implementation.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,6 +24,16 @@ function setupHooks() {
       }
     })
   });
+
+  Hooks.on("renderChatMessage", function (message, html, data) {
+    let audience = 'TODO';
+    let unkennyMarker = `
+      <p style="opacity: 0.5; font-size: 10px;">
+        Speaking with ${audience}
+      </p>
+    `;
+    html.find('.message-content').prepend(unkennyMarker);
+  });
 }
 
 setupHooks();
