2995690a6b853dbba4de92365a0bf403aec0b4f5
TheComamba <thecomamba@jojoheinze.de>
2024-09-27 15:42:07 +0200

    Updating update script to include subtree update.

--- foundry_scripts/update.sh
+++ foundry_scripts/update.sh
@@ -5,4 +5,8 @@ set -e
 # Change to git root directory
 cd "$(git rev-parse --show-toplevel)"
 
+# Update git subtree
+git subtree pull --prefix foundry_scripts FoundryScripts main --squash
+
+# Update npm packages
 npm update
