4bde320555d8b8d368652e73d488df8140ecdd1e
TheComamba <thecomamba@jojoheinze.de>
2024-09-27 15:42:17 +0200

    Squashed 'foundry_scripts/' changes from d28dda2..c5277f9
    c5277f9 Updating update script.
    27b1b7c Adjusting script path in release.sh.
    
    git-subtree-dir: foundry_scripts
    git-subtree-split: c5277f9081f0da21767a17fa2dadd1f777812662

--- release.sh
+++ release.sh
@@ -5,20 +5,20 @@ set -e
 # Change to git root directory
 cd "$(git rev-parse --show-toplevel)"
 
-if [ ! -d "scripts/venv" ]; then
-    python3 -m venv scripts/venv
+if [ ! -d "foundry_scripts/venv" ]; then
+    python3 -m venv foundry_scripts/venv
 fi
-if [ -f scripts/venv/bin/activate ]; then
-  source scripts/venv/bin/activate
+if [ -f foundry_scripts/venv/bin/activate ]; then
+  source foundry_scripts/venv/bin/activate
 else
-  source scripts/venv/Scripts/activate
+  source foundry_scripts/venv/Scripts/activate
 fi
 pip install python-dotenv > /dev/null
 pip install requests > /dev/null
 
 if [ "$1" = "--release" ]; then
-    python3 scripts/release.py --dry_run False
+    python3 foundry_scripts/release.py --dry_run False
 else
-    python3 scripts/release.py
+    python3 foundry_scripts/release.py
     echo "Dry run. Use argument --release to actually release."
 fi
